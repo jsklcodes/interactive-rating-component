7d872ddd22c6f30296c51a1701e915a81177686f
Kallyne Layane <hellookallyne@gmail.com>
2023-07-01 20:12:48 -0300

    Add functionality to activate the rating button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,22 @@
 const ratingCard = document.querySelector('#rating-card');
 const submitButton = document.querySelector('#submit-button');
 const ratingButtons = document.querySelectorAll('[data-rating]');
+
+let clickedRatingButton;
+let selectedRating;
+
+function toggleActiveRatingButton(ratingButton) {
+  if (clickedRatingButton) {
+    clickedRatingButton.classList.remove('button--active');
+  }
+
+  ratingButton.classList.add('button--active');
+  clickedRatingButton = ratingButton;
+  selectedRating = ratingButton.dataset.rating;
+}
+
+ratingButtons.forEach(ratingButton =>
+  ratingButton.addEventListener('click', _ =>
+    toggleActiveRatingButton(ratingButton)
+  )
+);
