152ab65d01f0c5c2ca3c795aa90ee5d0a408780b
Kallyne Layane <hellookallyne@gmail.com>
2023-07-01 20:07:38 -0300

    Add references to rating card elements

---
+++ assets/scripts/index.js
@@ -0,0 +1,3 @@
+const ratingCard = document.querySelector('#rating-card');
+const submitButton = document.querySelector('#submit-button');
+const ratingButtons = document.querySelectorAll('[data-rating]');
